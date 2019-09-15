5f3b83323e8e1b1107a22b682cde83090d1cc0e4
Ricardo Bittencourt <bluepenguin@gmail.com>
2019-09-15 18:43:46 -0300

    GD3 decoder

--- vgmdecoder.py
+++ vgmdecoder.py
@@ -1,8 +1,38 @@
 import sys
 
 class Vgm:
+  GD3_TAGS = [
+    'Track name (english)',
+    'Track name (japanese)',
+    'Game name (english)',
+    'Game name (japanese)',
+    'System name (english)',
+    'System name (japanese)',
+    'Author name (english)',
+    'Author name (japanese)',
+    'Date',
+    'File creator',
+    'Notes']
+
   def __init__(self, vgm):
     self.vgm = vgm
+    self.gd3 = self.parse_gd3()
+
+  def parse_gd3(self):
+    gd3_pointer = self.pointer(0x14)
+    if gd3_pointer == 0 or self.vgm[gd3_pointer : gd3_pointer + 4] != b'Gd3 ':
+      self.gd3 = None
+      return
+    size = self.uint32(gd3_pointer + 8)
+    raw_gd3 = self.vgm[gd3_pointer + 12 : gd3_pointer + 12 + size]
+    decoded_gd3 = raw_gd3.decode('utf-16').split('\0')
+    return dict(zip(Vgm.GD3_TAGS, decoded_gd3))
+
+  def uint32(self, offset):
+    return int.from_bytes(self.vgm[offset : offset + 4], byteorder='little')
+
+  def pointer(self, offset):
+    return self.uint32(offset) + offset
     
 def parse_vgm(filename):
   vgm = open(filename, "rb").read()
@@ -11,5 +41,6 @@ def parse_vgm(filename):
   return Vgm(vgm)
 
 vgm = parse_vgm(sys.argv[1])
-print(vgm)
+for tag, value in vgm.gd3.items():
+    print(tag, ':', value)
 
