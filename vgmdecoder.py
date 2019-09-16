f5aea97b079421733b2971d1753fc8fe90e5e369
Ricardo Bittencourt <bluepenguin@gmail.com>
2019-09-16 04:29:45 -0300

    Added NES APU

--- vgmdecoder.py
+++ vgmdecoder.py
@@ -1,5 +1,8 @@
 import sys
 
+class Dummy:
+  pass
+
 class Vgm:
   GD3_TAGS = [
     'Track name (english)',
@@ -17,6 +20,8 @@ class Vgm:
   def __init__(self, vgm):
     self.vgm = vgm
     self.gd3 = self.parse_gd3()
+    self.nes_clock = self.uint32(0x84)
+    self.data_offset = self.pointer(0x34)
 
   def parse_gd3(self):
     gd3_pointer = self.pointer(0x14)
@@ -31,11 +36,34 @@ class Vgm:
   def uint32(self, offset):
     return int.from_bytes(self.data(offset, 4), byteorder='little')
 
+  def uint16(self, offset):
+    return int.from_bytes(self.data(offset, 2), byteorder='little')
+
   def pointer(self, offset):
     return self.uint32(offset) + offset
 
   def data(self, offset, size):
     return self.vgm[offset : offset + size]
+
+  def parse_nes(self, nes):
+    current = self.data_offset
+    while (code := self.vgm[current]) != 0x66:
+      if (code & 0xF0) == 0x70:
+        print("wait %d" % (code - 0x70 + 1))
+        current += 1
+      elif code == 0x61:
+        print("wait %d" % self.uint16(current + 1))
+        current += 3
+      elif code == 0x62:
+        print("wait %d" % 735)
+        current += 1
+      elif code == 0xB4:
+        print("nes reg %02X val %02X" % (
+            self.vgm[current + 1], self.vgm[current + 2]))
+        current += 3
+      else:
+        print("unknown code %02X" % code)
+        return
     
 def parse_vgm(filename):
   vgm = open(filename, "rb").read()
@@ -43,7 +71,12 @@ def parse_vgm(filename):
     return None
   return Vgm(vgm)
 
+class NesApu:
+  def __init__(self):
+    pass
+
 vgm = parse_vgm(sys.argv[1])
 for tag, value in vgm.gd3.items():
     print(tag, ':', value)
-
+nes = NesApu()
+vgm.parse_nes(nes)
